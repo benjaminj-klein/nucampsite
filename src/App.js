c298bfd8931fd8608d952066e099db8f41d79504
Benjamin Klein <klein.benjaminj@gmail.com>
2023-11-25 14:40:31 -0700

    added featured partners

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import CampsiteDetailPage from './pages/CampsiteDetailPage';
+import AboutPage from './pages/AboutPage';
 
 function App() {
   return (
@@ -14,11 +15,13 @@ function App() {
         <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='contact' element={<ContactPage />} />
+          <Route path='about' element={<AboutPage />} />
           <Route path='directory' element={<CampsitesDirectoryPage />} />
           <Route
-                    path='directory/:campsiteId'
+                    path='directory/:campsiteId' //dynamic route
                     element={<CampsiteDetailPage />}
                 />
+          
         </Routes>
         <Footer/>
     </div>
